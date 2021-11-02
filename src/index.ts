0672b795a52fbfa1e81bd762c451b40393b68564
Tristan Defachel <defachel.tristan@gmail.com>
2021-11-02 15:57:53 +0100

    fix Ping 15:50

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import {
 } from "./ArgsParser"
 const isServer : boolean =  process.argv.indexOf('server') !== -1
 
-//const argsParser: IArgsParser = new Argspirser(process.argu)
+const argsParser:IArgsParser = new ArgsParser(process.argv)
 
-if(isServer) {
-    const port: number = argsParser.getListeningPort()
-    console.log('t Try Listening on 127.0.0.1: S port')
+if(argsParser.isServer()) {
+    const port:number = argsParser.getListeningPort();
+    console.log(`Try listening on 127.0.0.1:${port}`)
+
 } else if (process.argv.length >= 2) {
     const address : string = process.argv[2]
     console.log(address, process.argv);
